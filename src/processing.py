ef0741bb2da3082c3311894796ef33956a392d2e
Diana <ddmitrieva098@example.com>
2024-09-12 14:19:21 +1100

    A function that accepts a list of dictionaries

---
+++ src/processing.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+list_of_dicts = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
+    """Функция, которая принимает список словарей и опционально значение для ключа state
+    (по умолчанию 'EXECUTED')."""
+    return [d for d in list_of_dict if d.get("state") == state]
+
