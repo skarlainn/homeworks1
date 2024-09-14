ad5a305ad172e61765a47a603e5192aeaadc9bf9
Diana <ddmitrieva098@example.com>
2024-09-14 19:55:09 +1100

    add test_masks

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,10 @@
 def get_mask_card_number(card_number: str) -> str:
     """принимает на вход номер карты и возвращает ее маску."""
-    if len(card_number) == 16:
+    card_number = str(card_number)
+    if len(str(card_number)) == 16:
         return f"{card_number[0:3]} {card_number[4:6]}** **** {card_number[12:16]}"
     else:
-        return None
+        return "Ошибка ввода"
 
 
 def get_mask_account(account_number: str) -> str:
@@ -11,4 +12,4 @@ def get_mask_account(account_number: str) -> str:
     if len(account_number) == 20:
         return f"**{account_number[-4:]}"
     else:
-        return None
+        return "Ошибка ввода"
