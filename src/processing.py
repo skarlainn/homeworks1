24f1dc09d30ce28701ae8f7f6105e62c3ac07b03
Diana <ddmitrieva098@example.com>
2024-09-15 13:04:44 +1100

    add test_processing

---
+++ src/processing.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+list_of_dicts = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
+    """Функция, которая принимает список словарей и опционально значение для ключа state
+    (по умолчанию 'EXECUTED')."""
+    return [d for d in list_of_dict if d.get("state") == state]
+
+
+def sort_by_date(list_of_dict: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """Функция, которая принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате (date)."""
+    sorted_list = sorted(
+        list_of_dict,
+        key=lambda new_list_of_dict: new_list_of_dict["date"],
+        reverse=reverse,
+    )
+    return sorted_list
