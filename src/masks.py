93a00fcfb1b9365e00da1e55f3886c86a30eda24
Diana <ddmitrieva098@example.com>
2024-10-07 18:22:20 +1100

    fixes

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,36 @@
+import logging
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
     card_number = str(card_number)
     if card_number.isdigit() and len(card_number) == 16:
+        logger.info("Маскировка номера карты")
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
     else:
-        return "Ошибка ввода"
+        logger.warning(f"Неверный номер карты: длина номера - {len(card_number)}")
+        return []
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
     account_number = str(account_number)
     if account_number.isdigit() and len(account_number) == 20:
+        logger.info("Маскировка номера счета")
         return f"**{account_number[-4:]}"
     else:
-        return "Ошибка ввода"
+        logger.warning(f"Неверный номер счета: длина номера - {len(account_number)}")
+        return []
---
+++ src/utils.py
@@ -0,0 +1,48 @@
+import json
+import logging
+import os
+from json import JSONDecodeError
+from typing import Any
+
+from src.external_api import currency_conversion
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def financial_transactions(path: str) -> list:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    try:
+        logger.info("Открытие файла с транзакциями")
+        with open(path, encoding="utf-8") as financial_file:
+            try:
+                transactions = json.load(financial_file)
+            except JSONDecodeError:
+                return []
+        if not isinstance(transactions, list):
+            logger.error("Список транзакций пуст")
+            return []
+        logger.info("Создан список словарей с данными о финансовых транзакциях")
+        return transactions
+    except FileNotFoundError:
+        logger.error("Файл не найден")
+        return []
+
+
+def transaction_amount(trans: dict, currency: str = "RUB") -> Any:
+    """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
+    if trans["operationAmount"]["currency"]["code"] == currency:
+        amount = trans["operationAmount"]["amount"]
+        logger.info("Код валюты в транзакции RUB")
+    else:
+        amount = currency_conversion(trans)
+        logger.error("Код валюты транзакции не RUB, произведена конвертация")
+    return amount
